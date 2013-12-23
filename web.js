a6e7ca41ba08ea6679613344f6f61beb19f5a3d9
Matt Marcus <matt.g.marcus@gmail.com>
2013-12-23 03:29:33 +0000

    Adding in node capability

--- web.js
+++ web.js
@@ -3,6 +3,12 @@ var fs = require("fs");
 var htmlfile = "index.html";
 
 var app = express();
+app.configure(function () {
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use('/data', express.static(__dirname + '/data'));
+    app.use(app.router);    
+});
 
 app.get("/", function(req, res) {
     var html = fs.readFileSync(htmlfile).toString();
