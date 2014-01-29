6cda834c9488b74134627ac44df9d0c4a23afc15
Matt Marcus <matt.g.marcus@gmail.com>
2014-01-29 00:40:41 +0000

    Added in SEO stuff

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var fs = require("fs");
 var htmlfile = "index.html";
+var sitemapfile = "sitemap.xml";
 
 var app = express();
 app.configure(function () {
@@ -8,6 +9,11 @@ app.configure(function () {
     app.use(express.bodyParser());
     app.use('/data', express.static(__dirname + '/data'));
     app.use(app.router);    
+});
+
+app.get("/sitemap.xml", function(req, res) {
+    var html = fs.readFileSync(sitemapfile).toString();
+    res.send(html);
 });
 
 app.get("/*", function(req, res) {
